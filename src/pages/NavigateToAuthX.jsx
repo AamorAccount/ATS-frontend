d4afb7b3c2aa700a32830f3d09b9e948d0bb2abb
AamorAccount <blakshmi@aapmor.com>
2025-08-13 18:07:24 +0530

    Update NavigateToAuthX.jsx

--- src/pages/NavigateToAuthX.jsx
+++ src/pages/NavigateToAuthX.jsx
@@ -6,7 +6,7 @@ export default function NavigatetoAuthX() {
         const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URL) + '/callback'
         const domain = import.meta.env.VITE_DOMAIN
 
-        const authXUrl = `${import.meta.env.VITE_AUTHX_URL}?redirect_uri=${redirectUri}&domain=${domain}`
+        const authXUrl = `${import.meta.env.VITE_AUTHX_URL}?redirect_uri=${redirectUri}`
         window.location.href = authXUrl
     }, [])
 
