336702a8a37052405214a90fbf35e0d3af6a3498
AamorAccount <blakshmi@aapmor.com>
2025-08-13 17:41:49 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? `http://${import.meta.env.VITE_BACKEND_URL}/`
-      : "/",
+      : `${import.meta.env.VITE_BACKEND_URL}/`,
   // withCredentials: true,
 });
